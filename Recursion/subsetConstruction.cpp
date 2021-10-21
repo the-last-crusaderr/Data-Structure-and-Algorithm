d6f8e1b8c82e9f94fa7ffe2fe5e762c1c7e88585
the-last-crusaderr <guptasumit922@gmail.com>
2021-10-21 09:35:27 +0000

    solved subset construction

---
+++ Recursion/subsetConstruction.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+
+vector <int> subset;
+int n = 6 ;
+
+void search(int k){
+    if (k==n){
+        for(int i=0;i<subset.size();i++) {
+            cout << subset[i] << " ";
+        } 
+        cout << endl;
+    }
+    else{
+    search(k+1);
+    subset.push_back(k);
+    search(k+1);
+    subset.pop_back();
+    }
+}
+
+int main(){
+
+    search(2);
+
+/*
+    for(int i=0; i < subset.size(); i++){
+        cout << subset[i] << endl;
+    }
+*/
+
+    return 0;
+}
