b9a05ac5af8b9db8d02c5170d774a53902248136
the-last-crusaderr <guptasumit922@gmail.com>
2021-10-23 06:47:52 +0000

    implementation of insertion sort

---
+++ Sorting/insertionSort.cpp
@@ -0,0 +1,39 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+void insertionSort(int arr[],int n){
+
+
+    for(int i=1;i<n;i++){
+        for(int j=i;j>0;j--){
+            if(arr[j] < arr[j-1]  )
+                swap(arr[j],arr[j-1]);
+            else 
+                break;    
+        }
+    }
+}
+
+
+
+
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin >> arr[i];
+
+    insertionSort(arr,n);
+
+    for(int i=0;i<n;i++)
+        cout <<  arr[i] << " ";
+
+
+
+    return 0;    
+}
