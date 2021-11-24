0b1cc9e9ddc3b5fdb236dce97667b55646e5ac7b
the-last-crusaderr <guptasumit922@gmail.com>
2021-11-24 11:59:28 +0000

    understanding trees algorithm

---
+++ Trees/fundamentals.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+
+/*
+
+                  1
+                /   \                 
+               4     5                     
+
+
+*/
+
+
+
+
+
+
+
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+
+
+
+
+int main(){
+    Node* root;
+    root->data = 1;
+
+
+
+
+
+    return 0;
+}
+
+
